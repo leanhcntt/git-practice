a33284c365f08c8793bc7cbf50075e29be564d7d
leanhcntt <leanhcntt@gmail.com>
2020-12-07 14:08:55 +0700

    Update Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -31,21 +31,7 @@ namespace WebApp
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-
-			app.UseHttpsRedirection();
-
-			app.UseRouting();
-
-			app.UseAuthorization();
-
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllers();
-			});
+			
 		}
 	}
 }
